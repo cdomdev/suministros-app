a10fc1be695b9030597c04e0f873ff66f6ec17f5
cdomdev <dominguez5493.cd@gmail.com>
2024-09-29 17:41:48 -0500

    fix: ajuste del componentes de busqueda Se eliminan algunas salidas por consola en el componente

--- src/services/busqueda.ts
+++ src/services/busqueda.ts
@@ -1,7 +1,6 @@
 import axios from "axios"
 const API_HOST = "http://localhost:3000";
 export const busquedaProductos = async (searchTerm: string) => {
-    console.log('palabra que recibe la busqueda', searchTerm)
     try {
         const response = await axios.post(`${API_HOST}/busqueda-productos`, { query: searchTerm })
         return response
