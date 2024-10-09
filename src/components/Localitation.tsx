7a95682b309e5bba09257adbe77093523016a644
cdomdev <dominguez5493.cd@gmail.com>
2024-10-09 13:44:38 -0500

    fix:setting component location

--- src/components/Localitation.tsx
+++ src/components/Localitation.tsx
@@ -15,7 +15,7 @@ export const Localitation = () => {
     }, [])
 
     return (
-        <div className="flex flex-col items-center font-font-cust-2">
+        <div className="flex flex-col items-center">
             <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-map-pin size-8 stroke-[#6f6f6f]" width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#2c3e50" fill="none" strokeLinecap="round" strokeLinejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
