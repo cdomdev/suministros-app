004a3faa3fa0b62159002a6c0d0d85b61e22d68b
dominguez5493.cd <dominguez5493.cd@gmail.com>
2024-10-12 15:00:16 +0000

    refactor: add funtion remove data on forgot send mail

--- src/components/recuperarContraseña/SuccessRequest.tsx
+++ src/components/recuperarContraseña/SuccessRequest.tsx
@@ -1,15 +1,17 @@
 import { type DataForgotPassword } from "@/types/types";
 
-
 const SuccessRequest = ({ email }: DataForgotPassword) => {
-
+    
+    const removeData = () =>{
+        localStorage.removeItem('isSuccessSend');
+    }
     return (
         <div className="flex flex-col justify-center items-center">
             <h1 className="mb-2 font-text-base md:text-base text-center text-green-600">¡Tu informacion ah sido valida con exito!</h1>
             <p className="text-xs md:text-sm font-normal text-balance text-center">
                 En los proximos minutos enviaremos un correo a tu cuenta <strong>{email}</strong> con las instrucciones para ingresar una nueva contraseña
             </p>
-            <a href="/" className="my-2 text-center hover:underline duration-150">Volver a la pagina de inicio</a>
+            <button onClick={removeData} className="my-2 text-center hover:underline duration-150">Volver a la pagina de inicio</button>
         </div>
     );
 };
