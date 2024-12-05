3db928a0b5ad6fe9574ddcea53897f494a90c323
cdom <dominguez5493.cd@gmail.com>
2024-12-05 17:48:59 -0500

    refactor: remove code not funcion

--- src/components/UpdateSteps.tsx
+++ src/components/UpdateSteps.tsx
@@ -1,68 +1,78 @@
-import { useLocalStorage } from '@/hook/useLocalStoreage';
-import type { Producto } from '@/types/types';
-import { useEffect, useState } from 'react';
+import { useLocalStorage } from "@/hook/useLocalStoreage";
+import type { Producto } from "@/types/types";
+import { useEffect, useState } from "react";
 
 interface PropUpdate {
-    ruta: string,
-    bg: string,
-    textColor?: string,
-    textContent?: string,
-    type?: 'button' | 'submit' | 'reset';
-    disabled?: boolean;
+  ruta: string;
+  bg: string;
+  textColor?: string;
+  textContent?: string;
+  type?: "button" | "submit" | "reset";
+  disabled?: boolean;
 }
 
+export const UpdateSteps: React.FC<PropUpdate> = ({
+  ruta,
+  bg,
+  textColor,
+  textContent,
+  type = "button",
+  disabled = false,
+}) => {
+  const [productos, setProductos] = useState<Producto[]>([]);
+  const [, setSteps] = useLocalStorage("steps", 1);
 
-export const UpdateSteps: React.FC<PropUpdate> = ({ ruta, bg, textColor, textContent, type = 'button', disabled = false }) => {
-    const [productos, setProductos] = useState<Producto[]>([])
-    const [, setSteps] = useLocalStorage('steps', 1);
-    let rutaCapitalice;
+  if (ruta === "pago") {
+    return ruta.toLocaleLowerCase();
+  }
+  useEffect(() => {
+    const carrito = JSON.parse(localStorage.getItem("carrito") || "[]");
+    setProductos(carrito);
+  }, []);
 
-    if (ruta === 'pago') {
-        return rutaCapitalice = ruta.toLocaleLowerCase();
-    }
-    useEffect(() => {
-        const carrito = JSON.parse(localStorage.getItem('carrito') || '[]');
-        setProductos(carrito);
-    }, [])
-
-    const handleNextStep = (e: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement>) => {
-        if (disabled) {
-            e.preventDefault();
-            return;
-        }
-
-        setSteps((prev: number) => prev + 1);
-
-        if (type === 'button' || type === 'submit' || type === 'reset') {
-            window.location.href = `${ruta}`;
-        }
-    };
-
-
-    if (productos.length === 0) {
-        return <p className="text-sm text-center font-semibold my-2 text-red-400">No hay productos en tu carrito</p>
+  const handleNextStep = (
+    e: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement>
+  ) => {
+    if (disabled) {
+      e.preventDefault();
+      return;
     }
 
+    setSteps((prev: number) => prev + 1);
+
+    if (type === "button" || type === "submit" || type === "reset") {
+      window.location.href = `${ruta}`;
+    }
+  };
+
+  if (productos.length === 0) {
     return (
-        <>
-            {type === 'button' || type === 'submit' || type === 'reset' ? (
-                <button
-                    onClick={handleNextStep}
-                    type={type}
-                    disabled={disabled}
-                    className={`uppercase inline-block text-center text-xs md:text-sm border w-full py-2 rounded-md mb-2 hover:shadow-sm duration-150 mt-2 ${textColor} ${bg} ${disabled ? 'opacity-50 cursor-not-allowed' : ''}`}
-                >
-                    {textContent}
-                </button>
-            ) : (
-                <a
-                    href={ruta}
-                    onClick={handleNextStep}
-                    className={`uppercase inline-block text-center text-xs md:text-sm border w-full py-2 rounded-md mb-2 hover:shadow-sm duration-150 mt-2 ${textColor} ${bg}`}
-                >
-                    {textContent}
-                </a>
-            )}
-        </>
+      <p className="text-sm text-center font-semibold my-2 text-red-400">
+        No hay productos en tu carrito
+      </p>
     );
+  }
+
+  return (
+    <>
+      {type === "button" || type === "submit" || type === "reset" ? (
+        <button
+          onClick={handleNextStep}
+          type={type}
+          disabled={disabled}
+          className={`uppercase inline-block text-center text-xs md:text-sm border w-full py-2 rounded-md mb-2 hover:shadow-sm duration-150 mt-2 ${textColor} ${bg} ${disabled ? "opacity-50 cursor-not-allowed" : ""}`}
+        >
+          {textContent}
+        </button>
+      ) : (
+        <a
+          href={ruta}
+          onClick={handleNextStep}
+          className={`uppercase inline-block text-center text-xs md:text-sm border w-full py-2 rounded-md mb-2 hover:shadow-sm duration-150 mt-2 ${textColor} ${bg}`}
+        >
+          {textContent}
+        </a>
+      )}
+    </>
+  );
 };
--- src/utils/getDataFromStorage.ts
+++ src/utils/getDataFromStorage.ts
@@ -1,6 +1,6 @@
-import type { PropProfile } from "@/types/types";
+import type { DatosUsurio } from "@/types/types";
 
-export const getDataFromStorage = (): PropProfile | null => {
+export const getDataFromStorage = (): DatosUsurio | null => {
     const data = localStorage.getItem('infoProfileUSer');
     if (data) {
         return JSON.parse(data);
