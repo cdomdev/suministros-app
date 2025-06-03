6f01a6c6b973455ae523f65b43fd10f8b0b0cba8
cdomdev <dominguez5493.cd@gmail.com>
2025-06-02 19:17:05 -0500

    fix: change export to module.exports in tailwind configuration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
     "./node_modules/flowbite/**/*.js",
@@ -12,22 +12,18 @@ export default {
       "category-cocinas": "url('/cicinas.webp')",
       "category-contruccion": "url('/constr.webp')",
       "category-psisos": "url('/pisos.webp')",
-      "bg-chat": "linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(245,249,250,1) 35%, rgba(255,255,255,1) 100%);",
-
-
+      "bg-chat":
+        "linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(245,249,250,1) 35%, rgba(255,255,255,1) 100%);",
     },
-   
+
     gridTemplateColumns: {
       "cat-cust": "0.8fr 3fr",
       "col-cust-2": "50% 50%",
       "col-cust-3": "70% 30%",
       "col-cust-4": "60% 40%",
-      "col-cust-5": "25% 75%"
+      "col-cust-5": "25% 75%",
     },
-    // backgroundColor: {
-    //   'color-w': "rgb(37, 211, 102)"
-    // }
-
+   
   },
   plugins: [require("flowbite/plugin")],
 };
